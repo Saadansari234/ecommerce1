1bf19a1e2c7898e09aa218271debfb9276f7a7d6
Saadansari234 <saadansari3212@gmail.com>
2023-08-15 00:29:47 +0530

    mage an ecommerce website using js,jquery,slick slider cdn, have added addtocart scetion about a vegitable site

---
+++ ecommerce.js
@@ -0,0 +1,110 @@
+
+
+//cart1
+// cart-btn1
+
+
+let cart1=document.getElementById("cart1");
+cart1.style.display="none"
+
+let cart_btn1= document.getElementById("cart-btn1");
+
+cart_btn1.addEventListener("click", function(){
+    cart1.style.display="flex"
+
+})
+
+//cart2
+// cart-btn2
+
+let cart2=document.getElementById("cart2");
+cart2.style.display="none"
+
+let cart_btn2= document.getElementById("cart-btn2");
+
+cart_btn2.addEventListener("click", function(){
+    cart2.style.display="flex"
+
+})
+
+
+//cart3
+// cart-btn3
+
+let cart3=document.getElementById("cart3");
+cart3.style.display="none"
+
+let cart_btn3= document.getElementById("cart-btn3");
+
+cart_btn3.addEventListener("click", function(){
+    cart3.style.display="flex"
+
+})
+
+
+//cart4
+// cart-btn4
+
+let cart4=document.getElementById("cart4");
+cart4.style.display="none"
+
+let cart_btn4= document.getElementById("cart-btn4");
+
+cart_btn4.addEventListener("click", function(){
+    cart4.style.display="flex"
+
+})
+
+//cart5
+// cart-btn5
+
+let cart5=document.getElementById("cart5");
+cart5.style.display="none"
+
+let cart_btn5= document.getElementById("cart-btn5");
+
+cart_btn5.addEventListener("click", function(){
+    cart5.style.display="flex"
+
+})
+
+
+//cart6
+// cart-btn6
+
+let cart6=document.getElementById("cart6");
+cart6.style.display="none"
+
+let cart_btn6= document.getElementById("cart-btn6");
+
+cart_btn6.addEventListener("click", function(){
+    cart6.style.display="flex"
+
+})
+
+//cart7
+// cart-btn7
+
+let cart7=document.getElementById("cart7");
+cart7.style.display="none"
+
+let cart_btn7= document.getElementById("cart-btn7");
+
+cart_btn7.addEventListener("click", function(){
+    cart7.style.display="flex"
+
+})
+
+
+//cart8
+// cart-btn8
+
+let cart8=document.getElementById("cart8");
+cart8.style.display="none"
+
+let cart_btn8= document.getElementById("cart-btn8");
+
+cart_btn8.addEventListener("click", function(){
+    cart8.style.display="flex"
+
+})
